d8f0084b584275e40df0bd0239b5c79624165656
Antoine Rakotozafy <antoine.rakotozafy@gmail.com>
2016-09-20 20:05:17 -0700

    Delete c and compile c programs

--- 13b1eb.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-int main(int argc, char ** argv)
-{
-	int x = atoi(argv[1]);
-	printf("%.3f",cos(x*(M_PI/180)));
-	return 0;
-}
--- cc1e6c.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc,char ** argv)
-{
-	int x = atoi(argv[1]);
-	printf("%.5f",x/10.0);
-}
